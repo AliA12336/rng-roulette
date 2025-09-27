be07167aa3dd64aec3d8cf3ae44b32c898af9a1b
AliA12336 <amadams12336@gmail.com>
2025-09-27 15:19:57 -0400

    Fix plate

--- src/components/Plate.tsx
+++ src/components/Plate.tsx
@@ -4,8 +4,8 @@ export const Plate = ({children}: PropsWithChildren) => {
     return (
          <div className="plate-outside-border" style={{position: "relative", width: "350px", height: "350px", background: "#6f4e37", borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center", border: "5px solid #ffffff"}}>
             <div className="plate-outside" style={{width: "330px", height: "330px", background: "#ffffff", borderRadius: "50%", boxShadow: "inset 0 0 8px #ccc", display: "flex", justifyContent: "center", alignItems: "center"}}>
-              <div className="plate-inside-border" style={{position: "relative", width: "300px", height: "300px", background: "#6f4e37", borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center", border: "5px solid #ffffff"}}>
-                <div className="plate-inside" style={{width: "290px", height: "290px", background: "#ffffff", borderRadius: "50%", boxShadow: "inset 0 0 8px #ccc", display: "flex", justifyContent: "center", alignItems: "center"}}>
+              <div className="plate-inside-border" style={{position: "relative", width: "300px", height: "300px", background: "#6f4e37", borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center", border: "1px solid #ffffff"}}>
+                <div className="plate-inside" style={{width: "290px", height: "290px", background: "#ffffff", borderRadius: "50%",  display: "flex", justifyContent: "center", alignItems: "center"}}>
                     <>{children}</>
                 </div>
               </div>
