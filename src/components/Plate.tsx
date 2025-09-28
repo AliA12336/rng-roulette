3fea30bfcfa614413b8573ecfd0e99a0708aa88c
AliA12336 <amadams12336@gmail.com>
2025-09-28 15:08:53 -0400

    Fix details of plate and spinwheel ui components

--- src/components/Plate.tsx
+++ src/components/Plate.tsx
@@ -2,7 +2,7 @@ import { type PropsWithChildren } from "react"
 
 export const Plate = ({children}: PropsWithChildren) => {
     return (
-         <div className="plate-outside-border" style={{position: "relative", width: "600px", height: "600px", background: "#6f4e37", borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center", border: "5px solid #ffffff"}}>
+         <div className="plate-outside-border" style={{position: "relative", width: "580px", height: "580px", background: "#6f4e37", borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center", border: "5px solid #ffffff"}}>
             <div className="plate-outside" style={{width: "550px", height: "550px", background: "#ffffff", borderRadius: "50%", boxShadow: "inset 0 0 8px #ccc", display: "flex", justifyContent: "center", alignItems: "center"}}>
               <div className="plate-inside-border" style={{position: "relative", width: "540px", height: "540px", background: "#6f4e37", borderRadius: "50%", display: "flex", justifyContent: "center", alignItems: "center", border: "1px solid #ffffff"}}>
                 <div className="plate-inside" style={{width: "530px", height: "530px", background: "#ffffff", borderRadius: "50%",  display: "flex", justifyContent: "center", alignItems: "center"}}>
