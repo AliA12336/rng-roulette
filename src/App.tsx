eb5b1cfff581ed5a855ce1ae6a447f67676d41a0
AliA12336 <amadams12336@gmail.com>
2025-09-28 15:20:33 -0400

    Fix Provider placement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 import { CoffeeMug } from './components/Coffeemug'
 import { Plate } from './components/Plate'
-import { ChoicesProvider } from './hooks/ChoicesContext'
 import Layout from './Layout'
 
 function App() {
     return (
-      <ChoicesProvider>
         <Layout> 
           <div className="flex-1 flex items-center justify-center">
               <Plate>
@@ -14,7 +12,6 @@ function App() {
               </Plate>       
         </div>
         </Layout>
-      </ChoicesProvider>
   )
 }
 
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,14 +1,17 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/ui/app-sidebar"
+import { ChoicesProvider } from "./hooks/ChoicesContext"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider className="flex min-h-screen">
-      <AppSidebar />
-      <main className="flex-1 flex">
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
+    <ChoicesProvider>
+      <SidebarProvider className="flex min-h-screen">
+        <AppSidebar />
+        <main className="flex-1 flex">
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
+    </ChoicesProvider>
   )
 }
