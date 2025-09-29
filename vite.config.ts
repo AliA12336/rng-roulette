09c625eaf52ccc6a384f597e87117db896316cc2
AliA12336 <amadams12336@gmail.com>
2025-09-28 20:37:10 -0400

    Add deployment for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/rng-roulette/",
   plugins: [react(), tailwindcss(), VitePWA({
       registerType: "autoUpdate",
       includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
